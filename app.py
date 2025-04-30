dd54e7375e42dbb1481a296c9f46a330f7da9116
sreekarnakka <112407085+sreekarnakka@users.noreply.github.com>
2025-04-30 21:17:34 +0530

    Update Job Openings

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Senior Software Engineer',
+        'location': 'Bengaluru, India',
+        'salary': '10 LPA'
+    },
+    {
+        'id': 2,
+        'title': 'UX/UI Designer',
+        'location': 'Delhi, India',
+        'salary': '8 LPA'
+    },
+    {
+        'id': 3,
+        'title': 'Data Analyst',
+        'location': 'Chennai, India',
+        'salary': '12 LPA'
+    },
+    {
+        'id': 4,
+        'title': 'Backend Engineer',
+        'location': 'Remote',
+        'salary': '9 LPA'
+    }
+]
+
 @app.route("/")
-def hello_world():
-    return render_template('home.htm')
+def home():
+    return render_template("home.htm", jobs=JOBS)
 
 if __name__ == "__main__":
     app.run(debug=True)
